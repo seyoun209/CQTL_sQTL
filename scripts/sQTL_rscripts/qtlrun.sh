1f46696bd481205d5cc304f63cafd6b02d2f77f7
seyoun209 <byun.seyoun@gmail.com>
2024-09-17 16:30:25 -0400

    changed_gitignore

--- scripts/sQTL_rscripts/qtlrun.sh
+++ scripts/sQTL_rscripts/qtlrun.sh
@@ -1,16 +1,8 @@
-#for i in {{1..22},X,Y}
-#do
-#echo $i
-#sbatch qtlrun.sbatch $i
-#done
-
-
-#This is for the re-run adding the condition to the covariates
-
 for i in {{1..22},X,Y}
 do
 echo $i
-sbatch qtl_re_run_new_cov_condition.sbatch $i
+sbatch qtlrun.sbatch $i
 done
+
 
 
